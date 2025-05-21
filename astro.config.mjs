91ac8aa2e2eec6c2c73305b57d6486d402a3f2e5
SethMillan <millanseth@gmail.com>
2025-05-21 02:36:40 -0600

    add:pluggin typhography / env var / clientrouter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  // NECESITAS AGREGAR LO DE ABAJO PARA PODER USAR EL CLIENT ROUTER
+  // Habilitar el enrutador del cliente con prefetch
+  experimental: {
+    clientPrerender: true,
+  },
+  // Configuración del prefetch
+  prefetch: {
+    defaultStrategy: "viewport", // o 'hover' o 'tap'
+  },
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
 });
